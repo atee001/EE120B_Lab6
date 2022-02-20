939ffcde403c384e8f42692c8f4ca4ae3dffee4e
atee001 <atee001@ucr.edu>
2022-02-20 02:34:55 -0800

    Initial Commit

---
+++ atee001_lab6_part2.cpp
@@ -0,0 +1,208 @@
+const int b0 = 2;
+const int b1 = 3;
+const int b2 = 4;
+const int b3 = 5;
+const int b4 = 6;
+const int b5 = 7;
+const int b6 = 8;
+const int b7 = 9;
+
+int led_Pins[] = {2,3,4,5};
+
+void writeToB(unsigned char temp)
+{
+//Serial.println(temp);
+if ((temp>>7) & 0x01){digitalWrite(b7,HIGH);}
+if ((temp>>6) & 0x01){digitalWrite(b6,HIGH);}
+if ((temp>>5) & 0x01){digitalWrite(b5,HIGH);}
+if ((temp>>4) & 0x01){digitalWrite(b4,HIGH);}
+if ((temp>>3) & 0x01){digitalWrite(b3,HIGH);}
+if ((temp>>2) & 0x01){digitalWrite(b2,HIGH);}
+if ((temp>>1) & 0x01){digitalWrite(b1,HIGH);}
+if (temp & 0x01){digitalWrite(b0,HIGH);}
+}
+
+
+
+
+typedef struct task {
+  int state;
+  unsigned long period;
+  unsigned long elapsedTime;
+  int (*TickFct)(int);
+} task;
+
+const unsigned short tasksNum = 2;
+task tasks[tasksNum];
+enum LED_STATES{start, s_one_p, s_one_r, s_two_p, s_two_r, s_three_p, s_three_r, s_four_p, s_four_r, s_five_p, s_five_r} LED_STATE;
+enum LS_TWO_STATES{initial, s_one, s_two} LS_TWO;
+
+int TickFct_LS(int state){
+  
+  switch(state){    
+    case start: 
+        Serial.println("S0");
+        state =  s_one_p;      
+        break;
+    case s_one_p:
+        Serial.println("S1_p");
+        lightshow_one();      
+        state = s_two_p;      
+        break;
+    case s_two_p:
+        Serial.println("S2_p");
+        lightshow_two();      
+        state = s_three_p;
+        break;
+    case s_three_p:
+      Serial.println("S3_p");
+        lightshow_three();
+        state = s_four_p;
+        break;
+    case s_four_p:
+      Serial.println("S4_p");
+        lightshow_four();
+        state = s_five_p;
+        break;
+    case s_five_p:
+      Serial.println("S5_p");
+        lightshow_five();
+        state = s_one_p;
+        break;
+    }
+
+    return state;
+}
+
+int TickFct_LS_TWO(int state){
+
+  switch(state){
+    case initial:
+      digitalWrite(b4, HIGH);
+      digitalWrite(b5, HIGH);
+      digitalWrite(b6, HIGH);
+      digitalWrite(b7, HIGH);      
+      state = s_one;
+      break;
+     case s_one:
+      resetB();
+      digitalWrite(b5,HIGH);
+      digitalWrite(b6, HIGH);
+      state = s_two;
+      break;
+     case s_two:
+      resetB();
+      state = initial;
+      break;
+  }      
+    return state;  
+}
+
+
+void setup() {
+  unsigned char i = 0;
+  tasks[i].state = start;
+  tasks[i].period = 500;
+  tasks[i].elapsedTime = 0;
+  tasks[i].TickFct = &TickFct_LS;
+  i++;
+  tasks[i].state = initial;
+  tasks[i].period = 1000;
+  tasks[i].elapsedTime = 0;
+  tasks[i].TickFct = &TickFct_LS_TWO;
+  pinMode(A0, INPUT);
+  pinMode(b0, OUTPUT);
+  pinMode(b1, OUTPUT);
+  pinMode(b2, OUTPUT);
+  pinMode(b3, OUTPUT);
+  pinMode(b4, OUTPUT);
+  pinMode(b5, OUTPUT);
+  pinMode(b6, OUTPUT);
+  pinMode(b7, OUTPUT);
+  Serial.begin(9600);
+}
+
+void resetB()
+{
+digitalWrite(b7,LOW);
+digitalWrite(b6,LOW);
+digitalWrite(b5,LOW);
+digitalWrite(b4,LOW);
+digitalWrite(b3,LOW);
+digitalWrite(b2,LOW);
+digitalWrite(b1,LOW);
+digitalWrite(b0,LOW);
+}
+
+int power(int base, int expo){
+  int result = base;
+  if(expo == 0) return 1;
+  for(int i = 1; i < expo; i++){  
+    result *= base;
+  }
+  return result;
+}
+void lightshow_one(){
+  for(int i = 0; i < 4; i++){
+    if(digitalRead(A0)) return;
+    resetB();
+    writeToB(power(2,i)); 
+    delay(50);     
+  }  
+}
+
+void lightshow_two(){
+  for(int i = 3; i >= 0; i--){
+    if(digitalRead(A0)) return;
+    resetB();
+    writeToB(power(2,i)); 
+    delay(50);     
+  }  
+}
+
+void lightshow_three(){  
+  lightshow_one();
+  lightshow_two();  
+}
+
+void lightshow_four(){
+  for(int i = 2; i < 6; i++){
+    resetB();
+    digitalWrite(i, HIGH); digitalWrite(7-i, HIGH);
+    delay(50);
+  }  
+}
+
+void lightshow_five(){    
+  for(int i = 0; i < (sizeof(led_Pins)/2);i++){
+    if(digitalRead(A0)) return;
+    int bitmask = 255;
+    bitmask = ~(bitmask << i);
+    writeToB(bitmask);     
+    int count = 0;    
+      while(count < 3){ //control blinking of individual LED 
+      if(digitalRead(A0)) return;
+      digitalWrite(led_Pins[i], LOW);
+      delay(50);
+      digitalWrite(led_Pins[i], HIGH);      
+      delay(50);
+      ++count;    
+      }  
+      
+  }
+}
+
+
+void loop() {
+  resetB();
+  unsigned char i;
+  for (i = 0; i < tasksNum; ++i) {
+    if ( (millis() - tasks[i].elapsedTime) >= tasks[i].period) {
+        tasks[i].state = tasks[i].TickFct(tasks[i].state);
+        tasks[i].elapsedTime = millis(); // Last time this task was ran
+    }
+  }
+  delay(50);
+  
+  
+}
---
+++ atee001_lab6_part3.cpp
@@ -0,0 +1,134 @@
+#include <Servo.h>
+
+
+const int b0 = 2;
+const int b1 = 3;
+const int b2 = 4;
+const int b3 = 5;
+const int b4 = 6;
+const int b5 = 7;
+const int b6 = 8;
+const int b7 = 9;
+const int servoPin = 10;
+Servo myServo;
+
+typedef struct task {
+  int state;
+  unsigned long period;
+  unsigned long elapsedTime;
+  int (*TickFct)(int);
+} task;
+
+const unsigned short tasksNum = 1;
+task tasks[tasksNum];
+void resetA(){
+
+  digitalWrite(A0, LOW);
+  digitalWrite(A1, LOW);
+  digitalWrite(A2, LOW);
+  digitalWrite(A3, LOW);
+
+}
+enum Lock_States{start, lock_r, lock_p, unlock_p, unlock_r} L_state;
+
+
+int TickFct_LS(int state){
+ switch(state){
+  case start:
+    myServo.write(180);
+    state = lock_r;
+    break;
+  case lock_r:
+    state = (digitalRead(A0) && digitalRead(A3) && !digitalRead(A1) && !digitalRead(A2)) ? unlock_p : lock_r;
+    break;
+  case unlock_p:
+    myServo.write(0);
+    state = (digitalRead(A0) && digitalRead(A3) && !digitalRead(A1) && !digitalRead(A2)) ? unlock_p : unlock_r;
+    break;
+  case unlock_r:
+    state = (digitalRead(A0) && digitalRead(A3) && !digitalRead(A1) && !digitalRead(A2)) ? lock_p : unlock_r;
+    break;
+  case lock_p:
+    state = (digitalRead(A0) && digitalRead(A3) && !digitalRead(A1) && !digitalRead(A2)) ? lock_p : start;
+    break;
+ }
+  return state;
+}
+
+void setup(){
+  unsigned char i = 0;
+  tasks[i].state = start;
+  tasks[i].period = 100;
+  tasks[i].elapsedTime = 0;
+  tasks[i].TickFct = &TickFct_LS;
+  i++;
+  myServo.attach(servoPin);
+  myServo.write(180);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(b0, OUTPUT);
+  pinMode(b1, OUTPUT);
+  pinMode(b2, OUTPUT);
+  pinMode(b3, OUTPUT);
+  pinMode(b4, OUTPUT);
+  pinMode(b5, OUTPUT);
+  pinMode(b6, OUTPUT);
+  pinMode(b7, OUTPUT);
+  pinMode(servoPin, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void writeToB(unsigned long temp)
+{
+
+Serial.println(temp);
+
+if ((temp>>7) & 0x01){digitalWrite(b7,HIGH);}
+else digitalWrite(b7,LOW);
+if ((temp>>6) & 0x01){digitalWrite(b6,HIGH);}
+else digitalWrite(b6,LOW);
+if ((temp>>5) & 0x01){digitalWrite(b5,HIGH);}
+else digitalWrite(b5,LOW);
+if ((temp>>4) & 0x01){digitalWrite(b4,HIGH);}
+else digitalWrite(b4,LOW);
+if ((temp>>3) & 0x01){digitalWrite(b3,HIGH);}
+else digitalWrite(b3,LOW);
+if ((temp>>2) & 0x01){digitalWrite(b2,HIGH);}
+else digitalWrite(b2,LOW);
+if ((temp>>1) & 0x01){digitalWrite(b1,HIGH);}
+else digitalWrite(b1,LOW);
+if (temp & 0x01){digitalWrite(b0,HIGH);}
+else digitalWrite(b0,LOW);
+}
+
+void resetB()
+{
+digitalWrite(b7,LOW);
+digitalWrite(b6,LOW);
+digitalWrite(b5,LOW);
+digitalWrite(b4,LOW);
+digitalWrite(b3,LOW);
+digitalWrite(b2,LOW);
+digitalWrite(b1,LOW);
+digitalWrite(b0,LOW);
+}
+
+
+void loop(){
+
+//  myServo.attach(servoPin);
+//  myServo.write(map(voltage, 0, 1023, 0, 180));
+
+  unsigned char i;
+  for (i = 0; i < tasksNum; ++i) {
+    if ( (millis() - tasks[i].elapsedTime) >= tasks[i].period) {
+        tasks[i].state = tasks[i].TickFct(tasks[i].state);
+        tasks[i].elapsedTime = millis(); // Last time this task was ran
+    }
+  }
+  delay(100);
+
+}
